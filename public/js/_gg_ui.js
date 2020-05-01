6a0a5d3260300643b8f3717027e259d1320c8cf1
lauram88 <l.morinigo@samsung.com>
2020-05-01 16:54:03 +0100

    Migrated the creation of static cards into a whole function in _gg_ui.js

--- public/js/_gg_ui.js
+++ public/js/_gg_ui.js
@@ -202,3 +202,41 @@ let getActionsContent = (cActions) => {
     return actions;
 }
 
+// create static content
+
+let loadStaticCards = (cMainContent) =>{
+    const donateCard = createVerticalCard( 'Donate directly to the UNDP',
+    'Donate to the <a href=# id="undp-link">UNDP</a> to help tackle the root causes of poverty and create a better life for everyone.',
+     'images/raster/hands-vert.jpg',
+     0,
+     [['Donate','https://give.undp.org/give/120717/#!/donation/checkout']]);
+
+    cMainContent.appendChild(donateCard);
+
+    const learnCard = createVerticalCard('Learn about the Global Goals',
+        'World leaders agreed to 17 goals for a better world by 2030. These goals have the power to change the world and the people in it',
+        '/images/raster/goals-vert.jpg',                                
+        0,
+        [['Learn more','goals.html']]
+        );
+
+    cMainContent.appendChild(learnCard);
+    const peopleCard = createHorizontalCard('We the People',
+    'This star-studded video introduces the new plan for the people and planet: the UN Global Goals for Sustainable Development. Audio is in English.</p>',
+    '/images/raster/video-horz.jpg',
+    0,
+    [['Watch','https://youtu.be/RpqVmvMCmp0']]
+    );
+
+    cMainContent.appendChild(peopleCard);
+
+    const workCard   = createHorizontalCard('How this app works',
+    'When you see ads from this app, you will earn money for donations that support the Global Goals, such as fighting to end poverty, reducing social inequality, and tackling climate change.',
+    '/images/raster/boy-horz.jpg',
+    0,
+    null
+    );
+
+    cMainContent.appendChild(workCard);   
+}
+
--- public/js/updates.js
+++ public/js/updates.js
@@ -30,23 +30,7 @@ window.addEventListener('load', async () => {
     mainContent.appendChild(coronaCard);
   
 
-    const donateCard = createVerticalCard( 'Donate directly to the UNDP',
-                               'Donate to the <a href=# id="undp-link">UNDP</a> to help tackle the root causes of poverty and create a better life for everyone.',
-                                'images/raster/hands-vert.jpg',
-                                0,
-                                [['Donate','https://give.undp.org/give/120717/#!/donation/checkout']]);
-
-    mainContent.appendChild(donateCard);
-    
-    const learnCard = createVerticalCard('Learn about the Global Goals',
-                                'World leaders agreed to 17 goals for a better world by 2030. These goals have the power to change the world and the people in it',
-                                '/images/raster/goals-vert.jpg',                                
-                                0,
-                                [['Learn more','goals.html']]
-                                );
-    
-    mainContent.appendChild(learnCard);
-
+   
     const wallpaperCard = createWallpaperCard(`<p>${wallpaperJson.credits}</p>
                                                    <p>#GlobalGoal${wallpaperJson.goalNo}</p>
                                                    <a href="goal.html?no=${wallpaperJson.goalNo}" id="link-goal">Learn More</a>`,
@@ -57,23 +41,7 @@ window.addEventListener('load', async () => {
 
     mainContent.appendChild(wallpaperCard);
 
-    const peopleCard = createHorizontalCard('We the People',
-                                            'This star-studded video introduces the new plan for the people and planet: the UN Global Goals for Sustainable Development. Audio is in English.</p>',
-                                            '/images/raster/video-horz.jpg',
-                                            0,
-                                            [['Watch','https://youtu.be/RpqVmvMCmp0']]
-                                            );
-    
-    mainContent.appendChild(peopleCard);
-
-    const workCard   = createHorizontalCard('How this app works',
-                                   'When you see ads from this app, you will earn money for donations that support the Global Goals, such as fighting to end poverty, reducing social inequality, and tackling climate change.',
-                                    '/images/raster/boy-horz.jpg',
-                                    0,
-                                   null
-                                  );
-    
-    mainContent.appendChild(workCard);    
+    loadStaticCards(mainContent); 
     
 
     let linkGoal = document.querySelector('#link-goal');
@@ -82,7 +50,8 @@ window.addEventListener('load', async () => {
         window.document.location = './goal.html';
     }
 
-   /* link.onclick = function(){
+   /* Check Modal for UNDP link
+    link.onclick = function(){
         modal.style.display = "block";
     }
 
