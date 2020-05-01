e46a884f9924d214c79d14251992c6ca92953d18
lauram88 <l.morinigo@samsung.com>
2020-05-01 13:01:09 +0100

    Cards updated with new template functions

--- public/js/updates.js
+++ public/js/updates.js
@@ -1,21 +1,3 @@
-/*function loadCard(data){
-
-    if(data.type == 'WIDE'){
-        cardType = 'h-card-template';
-        img = '#template-img-horz'
-    }else{
-        cardType = 'v-card-template';
-        img = '#template-img-vert'
-    }  
-    let template = document.getElementById(cardType);
-    let templateContent = template.content;    
-    document.querySelector('#_main_content').appendChild(templateContent);
-    document.querySelector('slot[name="card-title"]').textContent = data.title;
-    document.querySelector('slot[name="card-text"]').textContent = data.message;
-    document.querySelector(img).src = data.assetUrl;
-}*/
-
-
 function getWallpapers(wallpapersJson){
     const arrayWallpapers = wallpapersJson.wallpapers;
     let wallpaperElement = document.getElementById('wallpaper');
@@ -54,39 +36,37 @@ window.addEventListener('load', async () => {
     /*Load Cards*/
    // createCard = (cLayoutType, cTitle, cContent, cImageUrl, cGGNum, cLink, cLanguage, cBadge) => {
 
-    const donateCard = createCard(CardLayoutType.VERTICAL, 'Donate directly to the UNDP',
+    const donateCard = createVerticalCard( 'Donate directly to the UNDP',
                                'Donate to the <a href=# id="undp-link">UNDP</a> to help tackle the root causes of poverty and create a better life for everyone.',
                                 'images/raster/hands-vert.jpg',
                                 0,
-                                '<a href="https://give.undp.org/give/120717/#!/donation/checkout">Donate</a>');
+                                [['Donate','https://give.undp.org/give/120717/#!/donation/checkout']]);
 
     mainContent.appendChild(donateCard);
     
-    const learnCard = createCard(CardLayoutType.VERTICAL,'Learn about the Global Goals',
+    const learnCard = createVerticalCard('Learn about the Global Goals',
                                 'World leaders agreed to 17 goals for a better world by 2030. These goals have the power to change the world and the people in it',
                                 '/images/raster/goals-vert.jpg',                                
                                 0,
-                                '<a href="goals.html">Learn more</a>'
+                                [['Learn more','goals.html']]
                                 );
     
     mainContent.appendChild(learnCard);
 
-    const peopleCard = createCard(CardLayoutType.HORIZONTAL,
-                                 'We the People',
-                                 'This star-studded video introduces the new plan for the people and planet: the UN Global Goals for Sustainable Development. Audio is in English.</p>',
-                                  '/images/raster/video-horz.jpg',
-                                  0,
-                                 '<a href="#">Watch</a>'
-                 );
+    const peopleCard = createHorizontalCard('We the People',
+                                            'This star-studded video introduces the new plan for the people and planet: the UN Global Goals for Sustainable Development. Audio is in English.</p>',
+                                            '/images/raster/video-horz.jpg',
+                                            0,
+                                            [['Watch','https://youtu.be/RpqVmvMCmp0']]
+                                            );
     
     mainContent.appendChild(peopleCard);
 
-    const workCard   = createCard(CardLayoutType.HORIZONTAL,
-                                   'How this app works',
+    const workCard   = createHorizontalCard('How this app works',
                                    'When you see ads from this app, you will earn money for donations that support the Global Goals, such as fighting to end poverty, reducing social inequality, and tackling climate change.',
                                     '/images/raster/boy-horz.jpg',
                                     0,
-
+                                   null
                                   );
     
     mainContent.appendChild(workCard);
