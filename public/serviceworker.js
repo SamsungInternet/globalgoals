eb9566dcd3192f4b2ee47acf0f5d4f75a864743b
lauram88 <l.morinigo@samsung.com>
2020-05-22 12:45:09 +0100

    Service Worker => fetch updated per each URL

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -9,6 +9,8 @@ var urlsToCache = [
     'images/raster/video-horz.jpg',
     'images/raster/boy-horz.jpg',
     'images/raster/goals_header.jpg',
+    'images/raster/covid_pandemic.jpg',
+    'images/raster/wallpaper-quote.png',
     'assets/goalsDetail.js',
     'js/_gg_ui.js',
 ];
@@ -24,23 +26,44 @@ self.addEventListener('install', function(event) {
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event){
+  const request = event.request;
+  /* Request Image */
+  if(request.headers.get('Accept').includes('image')){
+    event.respondWith(
+      caches.match(request)
+      .then(function(response){
+        return response || fetch(request)
+      })      
+      .catch( error => {
+        if(request.url.includes('/media/posts/covid')){
+          return caches.match('/images/raster/covid_pandemic.jpg')
+        }
+        if(request.url.includes('/media/wallpapers')){
+          return caches.match('/images/raster/wallpaper-quote.png')
+        }  
+
+        return caches.match('/images/raster/goals_header.jpg')
+      })
+    );
+    return;
+  }
+
+  //Everything else
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request)
-        .catch(error => {
-           return caches.match('images/raster/goals_header.jpg');
-        })
+    .then(function(response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
       }
-     
+      return fetch(event.request)
+        
+      }   
     )
-  );
-});
+  )
+})
+
 
 self.addEventListener('activate', function(event) {
 
