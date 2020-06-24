f1a0d2e8a3b8c275bd2e40fbb9c45263d7cd42c7
Ada Rose Cannon <ada@ada.is>
2020-06-24 12:26:22 +0100

    Use defer rather than window.load, run fetches in parallell so they do not block each other

--- public/js/updates.js
+++ public/js/updates.js
@@ -1,17 +1,17 @@
-window.addEventListener('load', async () => {
+(async function updates() {
     let link = document.getElementById('undp-link');
     let modal = document.getElementsByClassName('modal')[0];
     let close = document.getElementsByClassName('close')[0];
     let goalDiv = document.getElementsByClassName('goal-img')[0]; 
     let mainContent = document.getElementById('_main_content');
 
-    const wallpaper = await fetch('/wallpaper');
-    const wallpaperJson = await wallpaper.json();
-
-    const coronaCards = await fetch('/corona');
-    const coronaJson = await coronaCards.json();
-
-    
+    const [
+        wallpaperJson,
+        coronaJson
+    ] = await Promise.all([
+        fetch('/wallpaper').then(wallpaper => wallpaper.json()),
+        fetch('/corona').then(corona => corona.json())
+    ]);
 
     if (coronaJson.type == 'NORMAL'){
         coronaCard = createVerticalCard(coronaJson.title,
@@ -59,5 +59,4 @@ window.addEventListener('load', async () => {
         modal.style.display= "none";
     }*/
 
-})
-
+}())
