7226c07445dce57f5b16ccae9a26d181011f4822
lauram88 <l.morinigo@samsung.com>
2020-06-24 20:20:56 +0100

    Error Check if endpoints return empty, performance improved showing static data first

--- public/js/updates.js
+++ public/js/updates.js
@@ -1,65 +1,60 @@
-window.addEventListener('load', async () => {
-    let link = document.getElementById('undp-link');
-    let modal = document.getElementsByClassName('modal')[0];
-    let close = document.getElementsByClassName('close')[0];
-    let goalDiv = document.getElementsByClassName('goal-img')[0]; 
+async function loadDynamicCards(){
+
     let mainContent = document.getElementById('_main_content');
-
     
-    const wallpaper = await fetch('/wallpaper');
-    const wallpaperJson = await wallpaper.json();
-   
+    try {
+        const wallpaper =  fetch('/wallpaper');   
+        const coronaCards = fetch('/corona');
 
-    const coronaCards = await fetch('/corona');
-    const coronaJson = await coronaCards.json();
+        const wallpaperJson = await wallpaper.json();    
+        const coronaJson = await coronaCards.json();
 
-    
+        
 
-    if (coronaJson.type == 'NORMAL'){
-        coronaCard = createVerticalCard(coronaJson.title,
-                          coronaJson.message,
-                          coronaJson.assetUrl,
-                          coronaJson.goalNo,
-                          [[coronaJson.action,coronaJson.actionUrl]]);
-    }else{
-        coronaCard = createHorizontalCard(coronaJson.title,
-            coronaJson.message,
-            coronaJson.assetUrl,
-            coronaJson.goalNo,
-            [[coronaJson.action,coronaJson.actionUrl]]);
-    }
-
-    mainContent.appendChild(coronaCard);
+        if (coronaJson.type == 'NORMAL'){
+            coronaCard = createVerticalCard(coronaJson.title,
+                            coronaJson.message,
+                            coronaJson.assetUrl,
+                            coronaJson.goalNo,
+                            [[coronaJson.action,coronaJson.actionUrl]]);
+        }else{
+            coronaCard = createHorizontalCard(coronaJson.title,
+                coronaJson.message,
+                coronaJson.assetUrl,
+                coronaJson.goalNo,
+                [[coronaJson.action,coronaJson.actionUrl]]);
+        } 
   
 
    
-    const wallpaperCard = createWallpaperCard(`<p>${wallpaperJson.credits}</p>
-                                                   <p>#GlobalGoal${wallpaperJson.goalNo}</p>
-                                                   <a href="goal.html?no=${wallpaperJson.goalNo}" id="link-goal">Learn More</a>`,
-                                                   wallpaperJson.assetUrl,
-                                                   wallpaperJson.goalNo
-                                                );                                    
+        const wallpaperCard = createWallpaperCard(`<p>${wallpaperJson.credits}</p>
+                                                    <p>#GlobalGoal${wallpaperJson.goalNo}</p>
+                                                    <a href="goal.html?no=${wallpaperJson.goalNo}" id="link-goal">Learn More</a>`,
+                                                    wallpaperJson.assetUrl,
+                                                    wallpaperJson.goalNo
+                                                  );
+                                                  mainContent.appendChild(coronaCard);                                            
+                                                  mainContent.appendChild(wallpaperCard);
+         let linkGoal = document.querySelector('#link-goal');
+
+         linkGoal.onclick = function(){
+                window.document.location = './goal.html';
+         }                                             
+                                                  
+    }catch(error){
+        console.log(error);
+    }   
+
+                                                                                                         
                         
+ 
+}
 
-    mainContent.appendChild(wallpaperCard);
 
-    loadStaticCards(mainContent); 
-    
 
-    let linkGoal = document.querySelector('#link-goal');
-
-    linkGoal.onclick = function(){
-        window.document.location = './goal.html';
-    }
-
-   /* Check Modal for UNDP link
-    link.onclick = function(){
-        modal.style.display = "block";
-    }
-
-    close.onclick = function(){
-        modal.style.display= "none";
-    }*/
-
+window.addEventListener('load', () => {
+    let mainContent = document.getElementById('_main_content');
+    loadStaticCards(mainContent);   
 })
 
+loadDynamicCards();
