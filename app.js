efd115447f080a5d31191c9567c71647d7844e2d
lauram88 <l.morinigo@samsung.com>
2020-07-14 17:05:34 +0100

    Local Server check

---
+++ app.js
@@ -0,0 +1,93 @@
+
+const express = require('express');
+const fetch = require('node-fetch');
+const https = require('https');
+const app = express();
+require('dotenv').config();
+
+const port = process.env.PORT || 8080;
+const url = process.env.URL;
+
+app.listen(port, () => {
+  console.log(`Starting server at ${port}`);
+});
+
+app.use(express.static('public'));
+
+
+app.get('/wallpaper', async (req,res)=>{
+    const agent = new https.Agent({
+        rejectUnauthorized: false
+      });
+
+    console.log('entro');
+    const body = {
+        "key": process.env.API_KEY,
+        "uid": process.env.UID,
+        "language": ["en-UK"],
+        "deviceModel": "Samsung",
+        "salesCode": "",
+        "clientVersion": "1",
+        "country":"UK"
+      };
+
+
+
+    
+        const wallpapers_response = await fetch(url+'/api_pwa/v1/wallpapers/', {
+            agent,
+            method: 'post',
+            body:    JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' },
+
+        })
+        
+        const wallpapers_data = await wallpapers_response.json();
+        /*Retrieve One Random Wallpaper */
+        /*Filter only wallpapers with quotes */
+        let wallpaperQuotes = wallpapers_data.wallpapers.filter(wallpaper => wallpaper.type == 'QUOTE');
+        /*Get One Random*/
+        let wallpaper = wallpaperQuotes[Math.floor(Math.random() * wallpaperQuotes.length)];
+        
+      //  res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+        res.json(wallpaper)    
+   
+})
+
+app.get('/corona', async(req,res)=>{
+  const agent = new https.Agent({
+    rejectUnauthorized: false
+  });
+
+  const body = 
+    {
+      "key": process.env.API_KEY,
+      "uid": process.env.UID,
+      "country": "UK",
+      "language": ["en-EN"],
+      "deviceModel" : "Samsung",
+      "clientVersion" : "1.0",
+      "salesCode" : ""
+  }
+
+  const post_response = await fetch(url+'/api/v3/posts/', {
+        agent,
+        method: 'post',
+        body:    JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' },
+
+    }) 
+ 
+   const post_data = await post_response.json();
+
+   
+   /*Filter corona cards */
+   let coronaCards = post_data.posts.filter(card=>card.batch == 'covid' && card.screens == 'UPDATES');
+  
+   let coronaCard  = coronaCards[Math.floor(Math.random() * coronaCards.length)];
+
+   res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+   res.json(coronaCard);
+    
+  
+})
