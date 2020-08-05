1ee4d857434339bd25d6dab30ad83d2c563a9364
lauram88 <l.morinigo@samsung.com>
2020-08-05 15:01:55 +0100

    css performance changed and call to images to webp format

--- public/js/updates.js
+++ public/js/updates.js
@@ -51,7 +51,7 @@ async function loadWallpapers(){
             wallpaperCard = createWallpaperCard(`<p>Julia Gillard</p>
                                                         <p>#GlobalGoal4</p>
                                                         <a href="goal.html?no=4" id="link-goal">Learn More</a>`,
-                                                        '/images/raster/wallpaper-quote.png',
+                                                        '/images/raster/wallpaper-quote.webp',
                                                         4 
                                                       )
         }                                            
