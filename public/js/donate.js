d255462bc02580947cca1f0b43740e687160877a
lolaodelola <damzcodes@gmail.com>
2020-11-05 16:14:15 +0000

    stripe -wip

--- public/js/donate.js
+++ public/js/donate.js
@@ -1,5 +1,7 @@
+// import {loadStripe} from '@stripe/stripe-js';
+// const stripe = loadStripe('pk_test_qw1GN1EHv140IRz7N9Q6QcyX', {apiVersion: "2020-08-27"});
 
-
+const stripe = Stripe('pk_test_qw1GN1EHv140IRz7N9Q6QcyX');
 //creates the donation history UI chart 
 let createDonationChart = (data) => {
     let total = 0;
@@ -11,7 +13,7 @@ let createDonationChart = (data) => {
     header.setAttribute('class', 'donation-chart-header');
     header.innerHTML = "<h3>Everyone's donation history</h3>";
     chart.appendChild(header);
-    for(ggnum = 0; ggnum < data.length; ggnum++) {
+    for(let ggnum = 0; ggnum < data.length; ggnum++) {
         let line = `<div class="donation-chart-line">
         <img src="images/gg-tile/en/gg-${ggnum+1}.svg" class="donation-chart-icon gg-g${ggnum+1}">
         <span class="donation-chart-bar gg-g${ggnum+1}" style="width:${(data[ggnum]/total*100)}%"></span>
@@ -36,12 +38,28 @@ async function loadDonationsCard(){
 async function loadDirectDonation(amount, goalId){
     try{
         const directDonation = await fetch(`/directDonation/${amount}/${goalId}`);
-        const directDonationsJson = await directDonation.json();
-        console.log(directDonationsJson);
+        // const directDonationsJson = await directDonation.json();
+        // console.log(directDonationsJson);
 
     }catch(error){
         console.log(error);
     }
+}
+
+async function manageStripe(amount){
+    const paymentRequest = stripe.paymentRequest({
+        country: 'GB',
+        currency: 'gbp',
+        total: {
+            label: 'Demo total',
+            amount: amount * 100,
+        },
+        requestPayerName: true,
+        requestPayerEmail: true,
+    });
+
+    await paymentRequest.canMakePayment()
+    paymentRequest.show();
 }
 
 function getRandomInt(min, max) {
@@ -49,10 +67,18 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
 }
+const donateLink = document.createElement('a')
+donateLink.text = "DONATE NOW"
+donateLink.addEventListener('click', () => {
+    manageStripe(1)
+})
 
 window.addEventListener('load', () => {
     //adds the default donate card
     let mc = document.getElementById('_main_content');
+    const stripe = Stripe('pk_test_qw1GN1EHv140IRz7N9Q6QcyX', {
+        apiVersion: "2020-08-27",
+    });
     let defaultDonateCard = createVerticalCard('Donate directly to the UNDP', 'Donate to the UNDP to help tackle the root causes of poverty and create a better life for everyone.', '/images/raster/donate_vert.webp', 0, [['Donate', 'https://give.undp.org/give/120717/#!/donation/checkout']]);
     const amount = getRandomInt(5, 20);
     const goalId = 12; // should be dynamic
@@ -66,7 +92,7 @@ window.addEventListener('load', () => {
     );
 
     // mc.appendChild(defaultDonateCard);
-    mc.appendChild(specificDonateCard);
+    mc.appendChild(donateLink);
 });
 
 loadDirectDonation(20, 12)
