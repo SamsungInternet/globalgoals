76e86a2064e36f2fff29cb9c928bc395c29ab74e
lolaodelola <damzcodes@gmail.com>
2020-12-21 15:45:05 +0000

    check for correct err

--- public/js/donate.js
+++ public/js/donate.js
@@ -49,8 +49,10 @@ async function manageStripe(amount, goalId){
 
     await paymentRequest.canMakePayment().then(() => {
         paymentRequest.show();
-    }).catch(() => {
-        window.alert('Please add a payment card to your browser.')
+    }).catch((err) => {
+        if(err == `IntegrationError: Payment Request is not available in this browser.`){
+            window.alert('Please add a payment card to your browser.')
+        }
     })
 
     const clientSecret = await fetch(`/getPaymentIntent/${amount}`);
